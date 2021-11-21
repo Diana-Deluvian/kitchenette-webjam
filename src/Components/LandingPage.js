b54b2f0671ddd480b5488d0bc1304d71d0f3e5b4
Diana Deluvian <diana.deluvian@gmail.com>
2021-11-21 16:21:11 +0100

    cleaned up repository

--- src/Components/LandingPage.js
+++ src/Components/LandingPage.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import Blob from '../blob.svg';
-import Pie from '../pie.svg';
-import Pie2 from '../pie2.svg';
+import Roast from '../roast.svg';
 
 const LandingPage = () => {
     return (
         <div className="landing-page-container">
             <img src={Blob}/>
             <div id="absolute">
-            <img src={Pie2}/>
+            <img src={Roast}/>
             <h1>Diana's Kitchentte!</h1>
 
             </div>
