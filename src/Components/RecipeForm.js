f8e4b49533047b126711b13376ad48cd4f2b2049
Diana Deluvian <diana.deluvian@gmail.com>
2021-11-27 10:48:05 +0100

    added instructions and ingredients state

--- src/Components/RecipeForm.js
+++ src/Components/RecipeForm.js
@@ -9,6 +9,60 @@ const RecipeForm = ( props) => {
       setName(e.target.value);
     }
 
+    function addIngredient(e) {
+      e.preventDefault();
+      setIngredients([...ingredients, ""]);
+    }
+
+    const handleChangeIngredient = e => {
+      let newIngredient = e.target.value;
+      let newIngredients =  [...ingredients]
+      newIngredients[e.target.dataset.ingredientindex] = newIngredient;
+      setIngredients([...newIngredients]);
+  }
+
+  const deleteIngredient = e => {
+    e.preventDefault();
+    let newIngredients = [...ingredients];
+    newIngredients.splice(e.target.dataset.ingredientindex, 1);
+    setIngredients([...newIngredients])
+  }
+
+  const ingredientsList = ingredients
+  .map((ingredient, index) => (
+    <div key={`ingredient-${index}`} className="ingredients"index = {`test${index}`} >
+      <input data-ingredientindex={index} onChange={handleChangeIngredient} value={ingredient}></input>
+      <button data-ingredientindex={index}  onClick={deleteIngredient}>X</button>
+      </div>
+  ));
+
+  function addInstruction(e) {
+    e.preventDefault();
+    setInstructions([...instructions, ""]);
+  }
+
+  const handleChangeInstruction = e => {
+    let newInstruction = e.target.value;
+    let newInstructions =  [...instructions]
+    newInstructions[e.target.dataset.instructionindex] = newInstruction;
+    setInstructions([...newInstructions]);
+}
+
+const deleteInstruction = e => {
+  e.preventDefault();
+  let newInstructions = [...instructions];
+  newInstructions.splice(e.target.dataset.instructionindex, 1);
+  setInstructions([...newInstructions])
+}
+
+const instructionsList = instructions
+.map((instruction, index) => (
+  <div key={`instruction-${index}`} className="instructions"index = {`test${index}`} >
+    <input data-instructionindex={index} onChange={handleChangeInstruction} value={instruction}></input>
+    <button data-instructionindex={index}  onClick={deleteInstruction}>X</button>
+    </div>
+));
+
     const handleOnSubmit = (event) => {
         event.preventDefault();
         const recipe = { name, ingredients, instructions } 
@@ -21,6 +75,15 @@ const RecipeForm = ( props) => {
           Recipe name
           <input name="name" value={name} onChange={handleNameChange} />
         </label>
+        <div className="ingredients">
+        {ingredientsList}
+        <button onClick={addIngredient}>Add ingredient!</button>
+        </div>
+        <div className="instructionss">
+        {instructionsList}
+        <button onClick={addInstruction}>Add instruction!</button>
+        </div>
+        
         <input type="submit" value="Submit" />
       </form>
     )
