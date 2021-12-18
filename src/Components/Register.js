9339e39d9208136856a9560253e5ce944d857552
Diana Deluvian <diana.deluvian@gmail.com>
2021-12-18 15:02:32 +0100

    Register component

---
+++ src/Components/Register.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Register = () => {
+  return (
+    <div className='max-w-screen-lg flex flex-col items-center'>
+    <h1 className='font-emilysCandy text-6xl text-primary text-center mt-6'>
+      Register
+    </h1>
+    <p className='my-4 mx-6 xl:mx-0 max-w-80ch'>
+      Unfortunately, since this is my kitchenette, you aren't really allowed to register.
+      I mean, it wouldn't even make sense?
+    </p>
+    <p className='my-4 mx-6 xl:mx-0 max-w-80ch'>
+      But, if you are so determined to have an account, give me a job and I'll definitely allow it!
+      Alternatively, send me an amazon gift-card.
+    </p>
+  </div>
+  );
+};
+
+export default Register;
